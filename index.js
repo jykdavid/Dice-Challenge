f915a5d6eaf98722962b4f1a208a2621c12b8594
jykdavid <61402212+jykdavid@users.noreply.github.com>
2021-01-01 16:37:48 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,50 @@
+var randomNumber1 = Math.floor((Math.random() * 6)) + 1;
+var randomNumber2 = Math.floor((Math.random() * 6)) + 1;
+
+if(randomNumber1 == 1){
+    document.getElementsByClassName("img1")[0].setAttribute("src", "images/dice1.png");
+}
+else if(randomNumber1 == 2){
+    document.getElementsByClassName("img1")[0].setAttribute("src", "images/dice2.png");
+}
+else if(randomNumber1 == 3){
+    document.getElementsByClassName("img1")[0].setAttribute("src", "images/dice3.png");
+}
+else if(randomNumber1 == 4){
+    document.getElementsByClassName("img1")[0].setAttribute("src", "images/dice4.png");
+}
+else if(randomNumber1 == 5){
+    document.getElementsByClassName("img1")[0].setAttribute("src", "images/dice5.png");
+}
+else{
+    document.getElementsByClassName("img1")[0].setAttribute("src", "images/dice6.png");
+}
+
+if(randomNumber2 == 1){
+    document.getElementsByClassName("img2")[0].setAttribute("src", "images/dice1.png");
+}
+else if(randomNumber2 == 2){
+    document.getElementsByClassName("img2")[0].setAttribute("src", "images/dice2.png");
+}
+else if(randomNumber2 == 3){
+    document.getElementsByClassName("img2")[0].setAttribute("src", "images/dice3.png");
+}
+else if(randomNumber2 == 4){
+    document.getElementsByClassName("img2")[0].setAttribute("src", "images/dice4.png");
+}
+else if(randomNumber2 == 5){
+    document.getElementsByClassName("img2")[0].setAttribute("src", "images/dice5.png");
+}
+else{
+    document.getElementsByClassName("img2")[0].setAttribute("src", "images/dice6.png");
+}
+
+if(randomNumber1 == randomNumber2){
+    document.getElementsByTagName("h1")[0].innerHTML = "Draw!";
+}
+else if(randomNumber1 > randomNumber2){
+    document.getElementsByTagName("h1")[0].innerHTML = "Player 1 Wins!";
+}
+else{
+    document.getElementsByTagName("h1")[0].innerHTML = "Player 2 Wins!";
+}
